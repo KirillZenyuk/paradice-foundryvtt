274bbc2f2fb75c3b1bea5dd15106fd53046d7b96
Anessy <zenukirill@gmail.com>
2021-04-15 18:48:19 +0300

    Water texture Pinyon Script font

--- scripts/trident.js
+++ scripts/trident.js
@@ -1,5 +1,34 @@
 Hooks.on('diceSoNiceReady', (dice3d) => {
-    dice3d.addSystem({id: "ParaDice", name: "ParaDice"}, false);
+    dice3d.addSystem({id: "paradice-trident", name: "ParaDice — Trident"}, false);
+
+    dice3d.addTexture("water", {
+        name: "ParaDice — Water",
+        composite: "multiply",
+        source: "modules/paradice/graphics/water.png",
+    })
+        .then(() => {
+            dice3d.addColorset({
+                name: 'ParaDice — Trident colors',
+                description: "Gorgeous Water",
+                category: "ParaDice — Trident",
+                background: "#d4f1f9",
+                foreground: '#999',
+                outline: '#FFD600',
+                edge: '#FFEA00',
+                texture: 'water',
+                material: 'metal',
+                fontScale: {
+                    "d100":0.8,
+                    "d20": 0.9,
+                    "d12":1.0,
+                    "d10": 0.9,
+                    "d8": 0.9,
+                    "d6":1.2,
+                    "d2":1.3
+                },
+                font:"Pinyon Script"
+            },"no");
+        });
 
     dice3d.addDicePreset({
         type: "d20",
@@ -23,18 +52,18 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
             "17",
             "18",
             "19",
-            "graphics/icons/paradice-trident.png"
+            "modules/paradice/graphics/icons/paradice-trident.png"
         ],
-        system: "ParaDice"
+        system: "paradice-trident"
     }, "d20");
 
     dice3d.addDicePreset({
         type: "d2",
         labels: [
             "1",
-            "graphics/icons/paradice-trident.png"
+            "modules/paradice/graphics/icons/paradice-trident.png"
         ],
-        system: "ParaDice"
+        system: "paradice-trident"
     });
 
     dice3d.addDicePreset({
@@ -42,9 +71,9 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
         labels: [
             "1",
             "",
-            "graphics/icons/paradice-trident.png"
+            "modules/paradice/graphics/icons/paradice-trident.png"
         ],
-        system: "ParaDice",
+        system: "paradice-trident",
         fontScale: 1.3
     }, "df");
 
@@ -54,9 +83,9 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
             "1",
             "2",
             "3",
-            "graphics/icons/paradice-trident.png"
+            "modules/paradice/graphics/icons/paradice-trident.png"
         ],
-        system: "ParaDice"
+        system: "paradice-trident"
     }, "d4");
 
     dice3d.addDicePreset({
@@ -67,9 +96,9 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
             "3",
             "4",
             "5",
-            "graphics/icons/paradice-trident.png"
+            "modules/paradice/graphics/icons/paradice-trident.png"
         ],
-        system: "ParaDice"
+        system: "paradice-trident"
     }, "d6");
 
     dice3d.addDicePreset({
@@ -82,9 +111,9 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
             "5",
             "6",
             "7",
-            "graphics/icons/paradice-trident.png"
+            "modules/paradice/graphics/icons/paradice-trident.png"
         ],
-        system: "ParaDice"
+        system: "paradice-trident"
     }, "d8");
 
     dice3d.addDicePreset({
@@ -99,9 +128,9 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
             "7",
             "8",
             "9",
-            "graphics/icons/paradice-trident.png"
+            "modules/paradice/graphics/icons/paradice-trident.png"
         ],
-        system: "ParaDice"
+        system: "paradice-trident"
     }, "d10");
 
     dice3d.addDicePreset({
@@ -118,9 +147,9 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
             "9",
             "10",
             "11",
-            "graphics/icons/paradice-trident.png"
+            "modules/paradice/graphics/icons/paradice-trident.png"
         ],
-        system: "ParaDice"
+        system: "paradice-trident"
     }, "d12");
 
     dice3d.addDicePreset({
@@ -137,6 +166,6 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
             "90",
             "00",
         ],
-        system: "ParaDice"
+        system: "paradice-trident"
     }, "d10");
 })
